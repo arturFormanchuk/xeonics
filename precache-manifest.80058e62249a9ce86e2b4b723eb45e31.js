cf2f384e8dae0cd6ef25bca3aff9391694b153f3
arturFormanchuk <4manchuk@gmail.com>
2020-04-01 13:51:49 +0300

    Updates

---
+++ precache-manifest.80058e62249a9ce86e2b4b723eb45e31.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190b06107f3691687f445498a304ddda",
+    "url": "/xeonics/index.html"
+  },
+  {
+    "revision": "77d46b08f9ca78aac15d",
+    "url": "/xeonics/static/css/main.c1cc5a37.chunk.css"
+  },
+  {
+    "revision": "b60c9c1d30ff3ba0ca30",
+    "url": "/xeonics/static/js/2.fdf5bfe2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/xeonics/static/js/2.fdf5bfe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77d46b08f9ca78aac15d",
+    "url": "/xeonics/static/js/main.bcbd97d5.chunk.js"
+  },
+  {
+    "revision": "c4d6780be1356281ab5e",
+    "url": "/xeonics/static/js/runtime-main.468db3c1.js"
+  },
+  {
+    "revision": "0f8a486e2e7d40e49ce12d236d833e62",
+    "url": "/xeonics/static/media/AboutImg.0f8a486e.jpg"
+  },
+  {
+    "revision": "78e81a90b1bca04a6073629462813c2f",
+    "url": "/xeonics/static/media/xeo 2.78e81a90.JPG"
+  },
+  {
+    "revision": "f9b2dac2e16b2fa757af676ace34e20c",
+    "url": "/xeonics/static/media/xeo1logo.f9b2dac2.jpg"
+  }
+]);
